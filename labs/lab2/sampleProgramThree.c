a36c65a653459c44c1704e723414323ee8f869ba
gbaksa22 <gbaksa22@gmail.com>
2024-09-10 16:52:30 -0400

    lab 2

--- labs/lab2/sampleProgramThree.c
+++ labs/lab2/sampleProgramThree.c
@@ -4,6 +4,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
 int main()
 {
  // use these variables
@@ -16,9 +18,12 @@ int main()
     else if (pid == 0) {
         printf("I am child PID %ld\n", (long) getpid());
         /* insert an appropriate form of the exit() function here */
+        exit(0);
     }
     else {
         /* insert an appropriate form of the wait() system call here */
+        child = wait(&status);
+        printf("I am parent PID %ld\n", (long)getpid());
         printf("Child PID %ld terminated with return status %d\n", (long)
         child, status);
     }
