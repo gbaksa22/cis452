d14ea23f7ca19b809ba32347a0f3ecebed550ffa
gbaksa22 <gbaksa22@gmail.com>
2024-09-19 15:31:25 -0400

    outline

--- labs/lab4/programmingAssignment.c
+++ labs/lab4/programmingAssignment.c
@@ -4,3 +4,42 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+
+int main() {
+    /* 
+    • Input a string from the user (simulating the name of a file to access) 
+    • Spawn a child thread and communicate to it the filename 
+    requested/entered by the user 
+    • Immediately repeat the input/spawn sequence (i.e., accept a new file 
+    request) 
+    • Ensure your dispatcher is capable of properly handling rapidly entered 
+    filenames (i.e., the user rapidly entering the filenames in succession)*/
+}
+
+void *workerThread(void *arg) {
+    /*
+    • Obtain the simulated filename from the dispatcher 
+    • Sleep for a certain amount of time, simulating the time spent performing a 
+    file access: 
+        o with 80% probability, sleep for 1 second.  This simulates the 
+        scenario that the Worker thread has found the desired file in the 
+        disk cache and serves it up quickly. 
+        o with 20% probability, sleep for 7-10 seconds (randomly).  This 
+        simulates the scenario that the worker thread has not found the 
+        requested file in the disk cache and must block while it is read in 
+        from the hard drive. 
+    • Wake up, print a diagnostic message that includes the name of the file 
+    accessed, then terminate
+    */
+}
+
+void handleExit(int signum) {
+    /*
+    Your program should continue to execute until terminated by the user (^C).  At 
+    that point, your program should print out basic statistics: 
+    • Total number of file requests received 
+    When terminated, your program should cleanup as appropriate (i.e., free 
+    resources) and shutdown gracefully. If there are any executing ‘file requests’ 
+    those should complete before the program exits.
+    */
+}
