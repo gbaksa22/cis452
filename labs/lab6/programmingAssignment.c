3c0349a5df5ca7de1d5055b7a1cd4d79acd92099
Gabrielle <munsongabbi@gmail.com>
2024-10-03 14:42:19 -0400

    unsyncronized

--- labs/lab6/programmingAssignment.c
+++ labs/lab6/programmingAssignment.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/ipc.h>
+#include <sys/sem.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 
@@ -13,10 +14,22 @@ int main(int argc, char *argv[])
 {
     int status;
     long int i, loop, temp, *sharedMemoryPointer;
-    int sharedMemoryID;
+    int sharedMemoryID, semID;
     pid_t pid;
 
+    struct sembuf p = { 0, -1, SEM_UNDO};
+    struct sembuf v = { 0, +1, SEM_UNDO};
+
     loop = atoi(argv[1]);
+
+    semID = semget(IPC_PRIVATE, 1, S_IRUSR | S_IWUSR);
+    if (semID < 0) 
+    {
+        perror("Unable to obtain semaphore\n");
+        exit(1);
+    }
+
+    semctl(semID, 0, SETVAL, 1);
 
     sharedMemoryID = shmget(IPC_PRIVATE, SIZE, IPC_CREAT | S_IRUSR | S_IWUSR);
     if (sharedMemoryID < 0)
@@ -43,12 +56,22 @@ int main(int argc, char *argv[])
 
     if (pid == 0)
     { // Child
+
         for (i = 0; i < loop; i++)
         {
+            if (semop(semID, &p, 1) < 0) {
+                perror("semop wait failed");
+                exit(1);
+            }
             // swap the contents of sharedMemoryPointer[0] and sharedMemoryPointer[1]
             temp = sharedMemoryPointer[0];
             sharedMemoryPointer[0] = sharedMemoryPointer[1];
             sharedMemoryPointer[1] = temp;
+
+            if (semop(semID, &v, 1) < 0) {
+                perror("semop signal failed");
+                exit(1);
+            }
         }
         if (shmdt(sharedMemoryPointer) < 0)
         {
@@ -79,6 +102,8 @@ int main(int argc, char *argv[])
         perror("Unable to deallocate\n");
         exit(1);
     }
+
+    semctl(semID, 0, IPC_RMID);
 
     return 0;
 }
