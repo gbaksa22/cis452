7f0849073f67705f898efb13e76cdebdd5f471d4
gbaksa22 <gbaksa22@gmail.com>
2024-10-03 16:07:19 -0400

    working consumer + producer

--- assignments/assignment7-shared-memory/consumer.c
+++ assignments/assignment7-shared-memory/consumer.c
@@ -10,13 +10,14 @@
 typedef struct myStruct {
     int value;
     char message[40];
-};
-//Generate a shared key using the ‘ftok’ command by referencing ‘producer.c’ as the filename. 
-key_t key ftok('producer.c', 'W');
+} myStruct;
 
 int sharedMemoryID;
 struct myStruct *sharedMemoryPointer;
 int main() {
+    //Generate a shared key using the ‘ftok’ command by referencing ‘producer.c’ as the filename. 
+    key_t key = ftok("producer.c", 'W');
+
     // Create a shared memory segment using the ‘shmget’ command. 
     // The size of the shared memory segment should be the same size as the struct ‘myStruct’.
     // Use the following string for the permissions: “S_IWUSR|S_IRUSR|S_IRGRP|S_IWGRP|IPC_CREAT”.
@@ -26,22 +27,33 @@ int main() {
             perror("Unable to get shared memory"); 
             exit(1);
     }
+    // Attach to the memory segment using the ‘shmat’ command. Use the shmid generated from the shmget command
+    // Store the pointer in the variable ‘sharedMemoryPointer’. 
+    if ((sharedMemoryPointer = shmat(sharedMemoryID, 0, 0)) == (void *)-1) {
+            // Print an appropriate error message and exit the program if the above step fails.
+            perror("Unable to attach");
+            exit(1);
+    }
+    // Display both the integer value and the character string 
+    printf("Conusumer received message: %s\n", sharedMemoryPointer->message);
+    printf("Conusumer received value: %d\n", sharedMemoryPointer->value);
+
+    //Detach from the shared memory segment using the ‘shmdt’ command. 
+    if (shmdt(sharedMemoryPointer) < 0) {
+        perror("Unable to detach\n");
+        exit(1);
+    }
+
+    // Destroy the shared memory segment using ‘shmctl’ command with ‘IPC_RMID’ as the second option. 
+    if (shmctl(sharedMemoryID, IPC_RMID, 0) < 0) {
+        perror("Unable to deallocate\n");
+        exit(1);
+    }
+
     return 0;
 }
 
-/* 
-• Create a shared memory segment using the ‘shmget’ command. The size of the shared 
-memory segment should be the same size as the struct ‘myStruct’. Use the following 
-string for the permissions: “S_IWUSR|S_IRUSR|S_IRGRP|S_IWGRP|IPC_CREAT”. You 
-should store the ID (the return value) in the variable ‘sharedMemoryID’. 
-• Print an appropriate error message and exit the program if the above step fails. 
-• Attach to the memory segment using the ‘shmat’ command. Use the shmid generated 
-from the shmget command and store the pointer in the variable ‘sharedMemoryPointer’. 
-• Print an appropriate error message and exit the program if the above step fails. 
-• Display both the integer value and the character string 
-• Detach from the shared memory segment using the ‘shmdt’ command. 
-• Destroy the shared memory segment using ‘shmctl’ command with ‘IPC_RMID’ as the 
-second option. 
+/*
 • Compile and run your program. 
 • Try running the reader a second time, what is output and why? (include your answer in 
 the submission text) 
