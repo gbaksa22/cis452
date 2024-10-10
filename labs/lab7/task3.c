c1bd8a3006e71ec699f8747683479051599fc850
Gabrielle <munsongabbi@gmail.com>
2024-10-10 14:12:21 -0400

    camelCase

--- labs/lab7/task3.c
+++ labs/lab7/task3.c
@@ -4,14 +4,14 @@
 
 int main() {
     sem_t sem;
-    long max_value = 0;
+    long maxValue = 0;
 
     while (1) {
-        if (sem_init(&sem, 0, max_value) != 0) {
-            printf("Max counting semaphore value: %ld\n", max_value);
+        if (sem_init(&sem, 0, maxValue) != 0) {
+            printf("Max counting semaphore value: %ld\n", maxValue);
             break;
         }
-        max_value++;
+        maxValue++;
     }
     return 0;
 }
