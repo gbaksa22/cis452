5cba6bf2d259c2c750935629d1ccbf51a7ede77e
gbaksa22 <gbaksa22@gmail.com>
2024-10-24 13:47:13 -0400

    progress

--- labs/lab8/sampleProgramOne.c
+++ labs/lab8/sampleProgramOne.c
@@ -4,14 +4,38 @@
 
 #define SIZE 16
 
-int main()
+int main() 
 {
-    char *data1;
+    size_t current_size = SIZE;
+    char *data1 = malloc(current_size);
 
-    data1 = malloc(SIZE);
+    if (data1 == NULL) {
+        perror("Failed to allocate memory");
+        return 1;
+    }
+
     printf("Please input username: ");
-    scanf("%s", data1);
+
+    while (fgets(data1, current_size, stdin)) {
+        if (data1[strlen(data1) - 1] == '\n') {
+            data1[strlen(data1) - 1] = '\0';
+            break;
+        } else {
+            current_size *= 2;
+            char *temp = realloc(data1, current_size);
+
+            if (temp == NULL) {
+                perror("Failed to reallocate memory");
+                free(data1);
+                return 1;
+            }
+
+            data1 = temp;
+        }
+    }
+
     printf("You entered: [%s]\n", data1);
+
     free(data1);
     return 0;
 }
